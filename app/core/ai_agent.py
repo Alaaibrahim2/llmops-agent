af2418d7143e4db1cb297d7be18c3cbfde92d4ea
alaa ibrahim <oe911023@gmail.com>
2025-08-06 05:53:04 +0300

    Initial clean commit

---
+++ app/core/ai_agent.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+
+from langchain_groq import ChatGroq
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+
+from app.config.settings import settings
+
+def get_response_from_ai_agents(llm_id , query , allow_search ,system_prompt):
+
+    llm = ChatGroq(model=llm_id)
+
+    tools = [TavilySearchResults(max_results=2)] if allow_search else []
+
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        state_modifier=system_prompt
+    )
+
+    state = {"messages" : query}
+
+    response = agent.invoke(state)
+
+    messages = response.get("messages")
+
+    ai_messages = [message.content for message in messages if isinstance(message,AIMessage)]
+
+    return ai_messages[-1]
+
+
+
+
+
+
